b7c89d855ef059a98e5fb1138fb9975001dc7162
Serdar Geldyev <serdarabad@gmail.com>
2025-08-25 00:20:16 +0300

    styles added

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## Link to Vercel
+
+https://foodies-three-jade.vercel.app/
+
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Getting Started
--- components/main-header.tsx
+++ components/main-header.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
 import logoImg from "@/assets/logo.png";
+import styles from "./main-header.module.scss";
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
-        <Image src={logoImg.src} alt="logo" width={100} height={100} />
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
+        <Image src={logoImg.src} alt="logo" width={80} height={80} />
         Next Level Food
       </Link>
-      <nav>
+      <nav className={styles.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
