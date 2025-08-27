aa254a85cd5f15bcda01e8396236e21076fd9e17
Serdar Geldyev <serdarabad@gmail.com>
2025-08-28 00:03:24 +0300

    slideshow added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
+import Link from "next/link";
+import classes from "./page.module.scss";
+import ImageSlideshow from "@/components/images/image-slideshow";
+
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Time to get started!
-      </h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes["slideshow"]}>
+          <ImageSlideshow />
+        </div>
+        <div>
+          <div className={classes.hero}>
+            <h1>Super food for super foodies</h1>
+            <p>Taste and share snacks from all over the world</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="./community">Join Community</Link>
+            <Link href="./meals">Check out the meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={classes.section}>
+          <h2>Time to get started!</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+            esse, distinctio fugit debitis necessitatibus nostrum similique nisi
+            reprehenderit odit dolores perspiciatis fuga consequuntur nesciunt.
+            Vitae eius dolore saepe officia enim.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
